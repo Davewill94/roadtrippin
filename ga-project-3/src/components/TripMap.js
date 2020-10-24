51b3b397dfb95d756e5c7d35a314fe39db87da36
David Williams <williamsdavida@johndeere.com>
2020-10-23 20:34:54 -0500

    Got route to create, map to regen and map focus to adjust itself

--- ga-project-3/src/components/TripMap.js
+++ ga-project-3/src/components/TripMap.js
@@ -27,7 +27,8 @@ const Wrapper = styled.div`
                   {lat: 41.523529, lng: -90.577042},
                   {lat: 41.878113, lng: -87.629799}
             ],
-            routingControl: null
+            routingControl: null,
+            center: [42,-90]
           }
       }
     
@@ -48,13 +49,15 @@ const Wrapper = styled.div`
         this.setState({
             waypoints
         });
-
+        
+        this.map.remove();
+        this.adjustMap();
         this.genRoute();
     }
     
     genRoute = () => {
         this.removeRoute();
-        
+
         let routingControl = L.Routing.control({
             waypoints: [
                 L.latLng(this.state.waypoints[0].lat, this.state.waypoints[0].lng),
@@ -72,29 +75,50 @@ const Wrapper = styled.div`
         }
     }
 
+    adjustMap = () => {
+        let lats = 0;
+        let lngs = 0;
+        let center = [];
+        for(let i=0; i < this.state.waypoints.length; i++) {     
+            lats += this.state.waypoints[i].lat;
+            lngs += this.state.waypoints[i].lng;
+        }
+
+        center.push(lats/this.state.waypoints.length);
+        center.push(lngs/this.state.waypoints.length);
+        
+        this.setState({
+            center
+        })
+        this.genMap();
+    }
+
+    //generates map that centers around
+    genMap = () => {
+        this.map = L.map('map', {
+            center: this.state.center, 
+            zoom: 6,
+            zoomControl: false
+          });
+    
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            detectRetina: true,
+            maxZoom: 15, 
+            maxNativeZoom: 17,
+          }).addTo(this.map);
+    }
+
     componentDidMount() {
-      this.map = L.map('map', {
-        center: [42,-90], 
-        zoom: 6,
-        zoomControl: false
-      });
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        detectRetina: true,
-        maxZoom: 15, 
-        maxNativeZoom: 17,
-      }).addTo(this.map);
-
-      this.genRoute();
+        this.genMap();
     }
 
     render() {
-      return (
-          <div>
-            <Wrapper width="600px" height="200px" id="map" />
-            <Destinations tripSubmit={this.tripSubmit}/>
-          </div>
-)
+        return (
+            <div>
+                <Wrapper width="600px" height="200px" id="map" />
+                <Destinations tripSubmit={this.tripSubmit}/>
+            </div>    
+        )
     }
   }
     
