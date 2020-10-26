81de2f88aabdd9e453770a0d6caf2326fba1adb6
MD Manning <manningmichaeld@johndeere.com>
2020-10-26 15:12:37 -0500

    styling adjustments mainly

--- ga-project-3/src/components/Header.js
+++ ga-project-3/src/components/Header.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+
 
 
 const Header = () => {
     return (
         <div id="header">
             <h1>RoadTrippin</h1>
-            <nav>
+            {/* <nav>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/newTrip">New Trip</Link>
                 <Link to="/contacts">Contacts</Link>
+            </nav> */}
+            <nav>
+                <NavLink to='/home' activeClassName="active">Home</NavLink>
+                <NavLink to='about' activeClassName="active">About</NavLink>
+                <NavLink to='/newTrip' activeClassName="active">New Trip</NavLink>
+                <NavLink to='contacts' activeClassName="active">Contacts</NavLink>
             </nav>
-
         </div>
     )
 }
