94b9c8d187137f81fc6d2b772019d2f828591da4
MD Manning <manningmichaeld@johndeere.com>
2020-10-28 15:24:25 -0500

    got the changes working

--- ga-project-3/src/App.js
+++ ga-project-3/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Route path="/home" >
+      <Route exact path={["/home","/"]} >
           <HomePage />
       </Route>
 
--- ga-project-3/src/components/About.js
+++ ga-project-3/src/components/About.js
@@ -12,12 +12,12 @@ justify-content: start;
 const TextDiv = styled.div`
 display: flex;
 text-align: left;
-width: 750px;
+width: 900px;
 height:auto;
 font-size: 18px;
 padding: 15px;
 border-radius: 25px;
-margin: 100px 100px 0 -200px;
+margin: 30px 80px 0 80px;
 border: 2px solid rgb(60, 60, 60);
 background: rgba(237, 248, 255, 0.687);
 color: black;
@@ -25,7 +25,29 @@ color: black;
 p {
    line-height: 175%;
 }
+
+img {
+    visibility:hidden;
+    position: fixed;
+    right: 250px;
+    bottom: 90px;
+    height: 150px;
+
+}
+
+h4 {
+    display: inline-block;
+}
+
+h4:hover + img{
+        visibility:visible;
+}
+
+span {
+    width: 50px;
+}
 `
+
 
 
 export default function About() {
@@ -34,7 +56,10 @@ export default function About() {
         <WrapperDiv>
             <TextDiv>
             <p>
-                <h4>Roadtrip Soundtrack </h4>
+                <span class="overlay"><h4>Roadtrip Soundtrack</h4>
+                <img id="tripGif" src="https://thumbs.gfycat.com/DetailedIllfatedArawana.webp" alt="manning"></img> 
+
+                </span><br></br>
                 Have you ever started a road trip and struggled to find the right podcast or album to listen to? 
                 With this app, you can plan your route, view estimated travel times, and create your roadtrip soundtrack ahead of time. 
                  <br></br>
@@ -42,23 +67,29 @@ export default function About() {
                 <br></br>
                 We will provide some useful information such as total trip time, estimated travel time,
                 and average gas consumption.
-                <h4>Spotify API:</h4>
+                <span class="overlay"><h4>Spotify API:</h4>
+                <img id="tripGif" src="https://media.tenor.com/images/6cc3abb6d3a1562319e217eb626612a5/tenor.gif" alt="manning"></img> 
+
+                </span><br></br>
                 <p>This APP utilizes the Spotify API which gives you access to the "danceability, energy, and loudness" of songs.
                     These keywords allow you to narrow in on the exact type of roadtrip soundtrack you are looking for. 
                                
-                <a href="https://developer.spotify.com/documentation/web-api/" target="_blank">  Spotify API</a>
+                <a href="https://developer.spotify.com/documentation/web-api/" target="blank">  Spotify API</a>
                 </p> 
 
-                <h4>LeafLet API:</h4>
+                <span class="overlay"><h4>Leaflet API:</h4>
+                <img id="tripGif" src="https://media0.giphy.com/media/d2jjuAZzDSVLZ5kI/giphy.gif" alt="manning"></img> 
+
+                </span><br></br>
                 <p>The map portion of this app was creating using Leaflet React. <br></br>
                 One issue with using leaflet is the server seemed to go down every day between 11am-2pm. 
-                <a href="https://react-leaflet.js.org/?_sm_au_=iVVF1T4WqPf0JLJNL321jK0f1JH33" target="_blank">React Leaflet API</a>
+                <a href="https://react-leaflet.js.org/?_sm_au_=iVVF1T4WqPf0JLJNL321jK0f1JH33" target="blank">React Leaflet API</a>
                 </p>
 
                 <p>Other Helpful Roadtrip Planning Links</p>
-                <a href="http://www.ultimatecampgrounds.com/index.php/products/full-map" target="_blank">ultimagecampgrounds.com</a><br></br>
-                <a href="https://www.fueleconomy.gov/trip/" target="_blank">fueleconomy.gov </a><br></br>
-                <a href="https://roadtrippers.com/" target="_blank">roadtrippers.com</a>
+                <a href="http://www.ultimatecampgrounds.com/index.php/products/full-map" target="blank">ultimagecampgrounds.com</a><br></br>
+                <a href="https://www.fueleconomy.gov/trip/" target="blank">fueleconomy.gov </a><br></br>
+                <a href="https://roadtrippers.com/" target="blank">roadtrippers.com</a>
             </p>
             </TextDiv>
         </WrapperDiv>
