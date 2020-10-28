d631073c5948ade16341a6db1265d474c3c5b98d
jonnyv91 <vaflorjonathan@johndeere.com>
2020-10-28 12:36:36 -0500

    added .env files to store keys locally and passed them into the app

--- ga-project-3/src/components/Spotify/Credentials.js
+++
@@ -1,9 +0,0 @@
-const Credentials = () => {
-
-    return {
-        ClientId: '6923a253a7f2467f9c4c8e32f34f522b',
-        ClientSecret: '8ce8667178a541378e18666fa2ec9a5b'
-    }
-}
-
-export { Credentials };
